23e368e15cb7ea4639ad3adc27aa2dc986ddce54
sebastian prado <sebaslkjh@gmail.com>
2022-08-05 13:30:12 -0500

    creando routes-index

--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -1,9 +1,14 @@
+const express = require("express");
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const countryRouter = require("./countryRouter");
+const activityRouter = require("./activityRouter")
 
 const router = Router();
+router.use(express.json());
+router.use("/countries", countryRouter)
+ router.use("/activity",activityRouter)
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
