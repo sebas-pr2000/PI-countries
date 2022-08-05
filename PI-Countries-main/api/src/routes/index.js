e50e4915654d3a512a802b31ecc21196c1108968
sebastian prado <sebaslkjh@gmail.com>
2022-08-05 15:56:05 -0500

    creando routes para filter

--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -3,12 +3,14 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const countryRouter = require("./countryRouter");
-const activityRouter = require("./activityRouter")
+const activityRouter = require("./activityRouter");
+const filterRouter = require("./filterRoutes");
 
 const router = Router();
 router.use(express.json());
-router.use("/countries", countryRouter)
- router.use("/activity",activityRouter)
+router.use("/countries", countryRouter);
+router.use("/activity",activityRouter);
+router.use("/filter", filterRouter);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
