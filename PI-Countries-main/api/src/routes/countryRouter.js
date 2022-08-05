7ecf5a8f4a292d7ef5c65200a6e7645416ce2cf8
sebastian prado <sebaslkjh@gmail.com>
2022-08-05 15:21:12 -0500

    añadiendo get-id y get-name

--- PI-Countries-main/api/src/routes/countryRouter.js
+++ PI-Countries-main/api/src/routes/countryRouter.js
@@ -1,6 +1,20 @@
 const {Router} = require("express");
-const{allCountries} = require("../controllers/country")
+const{allCountries, detailCountry, countryName} = require("../controllers/country")
 const router = Router();
+
+router.get("/?", async (req , res)=>{
+  const {name } = req.query;
+
+  try {
+  const country = await countryName(name);
+  res.status(200).json(country)    
+    
+  } catch (error) {
+    res.status(404).json({error: error.message})
+  }
+ 
+})
+
 
 router.get("/", async (req, res)=>{
 
@@ -11,6 +25,20 @@ router.get("/", async (req, res)=>{
     } catch (error) {
         res.status(404).json({eror: error.message})
     }
+});
+
+
+
+router.get("/:id", async (req, res)=>{
+  const {id} = req.params;
+
+  try {
+      const detail = await detailCountry(id)
+      res.status(200).json(detail)
+    
+  } catch (error) {
+    res.status(404).json({error: error.message})
+  }
 });
 
 
