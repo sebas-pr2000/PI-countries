55c4d2985f9a3571129acf3fbbaa3235ca26e168
sebastian prado <sebaslkjh@gmail.com>
2022-08-05 13:37:30 -0500

    creando carpeta controllers

---
+++ PI-Countries-main/api/src/controllers/activity.js
@@ -0,0 +1,44 @@
+const { Country, Activity } = require('../db.js');
+
+const createActivity = async (name, difficulty, duration, season, countries)=>{
+
+   try {// para poder verla en sql SELECT * FROM activities;
+     let activity = await Activity.create({ 
+         name, 
+         difficulty,
+         duration,
+         season
+      });
+    
+      let searchCountry = await Country.findAll({ // buscamos el pais que pertenece la activitidad,para relacionarla
+         where:{
+            name: countries
+         }
+      });
+
+      return activity.addCountry(searchCountry)
+
+
+   } catch (error) {
+      console.log(`Error createActivity ${error} `)
+   }
+   
+}
+
+const getActivity = async ()=>{
+
+   try {
+      const activities = await Activity.findAll();
+      return activities;
+
+   } catch (error) {
+      console.log(error)
+   }
+
+}
+
+
+module.exports = {
+    createActivity,
+    getActivity
+}
---
+++ PI-Countries-main/api/src/controllers/country.js
@@ -0,0 +1,54 @@
+const { Country, Activity } = require('../db.js');
+const  axios = require("axios");
+
+
+// SET client_encoding ='UTF8'; codigo error bytes
+
+
+const allCountries = async ()=>{
+    // primero debo trar los paises con la informacion requerida de la api   -- Listo
+    // crear por cada una un nuevo pais en la base de datos con el CREATE    -- listo
+    // retornar todos los paises  FIND ALL 
+    
+  // llamar paises de la API externa y mandarla a la BD
+try {
+  const countries = await Country.findAll();
+
+  if(countries.length === 0){
+
+    await axios.get('https://restcountries.com/v3/all')
+   .then(response =>(response.data.forEach(e => Country.create({ 
+     ID : e.cca3 ,
+     name: e.name.common,
+     Image : e.flags[1] || "image country",
+     continent: e.continents[0] ,
+     capital: e.capital ? e.capital[0] : "not capital",
+     subregion: e.subregion, 
+     area: e.area , 
+     population: e.population  
+   })
+   ))) 
+
+   return (countries);
+ 
+  }else{
+    
+    return(countries);
+
+  }
+
+
+} catch (error) {
+  console.log(error);
+}
+  
+   
+  
+}
+
+// 
+module.exports = {
+    allCountries
+}
+
+// recordar que se puede hacer "||" para que un elemento tenga un valor por default
