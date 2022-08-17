da31fb9ec296dcfc1437f3c9da417602031d6fb4
sebastian prado <sebaslkjh@gmail.com>
2022-08-17 15:53:37 -0500

    creacion de routes

--- PI-Countries-main/client/src/App.js
+++ PI-Countries-main/client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage.jsx";
+import Home from './components/Home/Home.jsx';
+import CreateActivity from "./components/CreateActivity/CreateActivity.jsx";
+import Detail from "./components/Detail/Detail";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Henry Countries</h1>
+      <Switch>
+        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/home" component={Home} />
+        <Route path="/activity" component={CreateActivity}/>
+        <Route path="/home/:ID" component={Detail}/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
