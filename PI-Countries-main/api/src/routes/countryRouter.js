b636763842731d6f287083f760a16a4c8bc9a9a7
sebastian prado <sebaslkjh@gmail.com>
2022-08-17 16:09:50 -0500

    separando countryRouter

--- PI-Countries-main/api/src/routes/countryRouter.js
+++ PI-Countries-main/api/src/routes/countryRouter.js
@@ -1,26 +1,26 @@
 const {Router} = require("express");
-const{allCountries, detailCountry, countryName} = require("../controllers/country")
+const{allCountries, allNamesCountries} = require("../controllers/country")
 const router = Router();
 
-router.get("/?", async (req , res)=>{
-  const {name } = req.query;
-
-  try {
-  const country = await countryName(name);
-  res.status(200).json(country)    
-    
-  } catch (error) {
-    res.status(404).json({error: error.message})
-  }
- 
-})
 
 
 router.get("/", async (req, res)=>{
 
     try {
         const countries = await allCountries();
+        console.log("se pidio countries")
             res.status(200).json(countries)   
+        
+    } catch (error) {
+        res.status(404).json({eror: error.message})
+    }
+});
+
+router.get("/names", async (req, res)=>{
+
+    try {
+        const countriesNames = await allNamesCountries();
+            res.status(200).json(countriesNames)  
         
     } catch (error) {
         res.status(404).json({eror: error.message})
@@ -29,18 +29,6 @@ router.get("/", async (req, res)=>{
 
 
 
-router.get("/:id", async (req, res)=>{
-  const {id} = req.params;
-
-  try {
-      const detail = await detailCountry(id)
-      res.status(200).json(detail)
-    
-  } catch (error) {
-    res.status(404).json({error: error.message})
-  }
-});
-
-
-
+  
+  
 module.exports = router
