ad924482ada382f8dc52627bf10b9657222b7168
sebastian prado <sebaslkjh@gmail.com>
2022-08-17 16:12:57 -0500

    definiendo router para countriesSRouter

--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -3,12 +3,15 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const countryRouter = require("./countryRouter");
+const countrySRouter = require("./CountrySRouter")
 const activityRouter = require("./activityRouter");
 const filterRouter = require("./filterRoutes");
+
 
 const router = Router();
 router.use(express.json());
 router.use("/countries", countryRouter);
+router.use("/countries/s", countrySRouter);
 router.use("/activity",activityRouter);
 router.use("/filter", filterRouter);
 
